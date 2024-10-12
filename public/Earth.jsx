06057e346117064deda2465362bd62a1b259aa3f
Gaspar Bonari <gasparbonari@gmail.com>
2024-10-12 15:46:52 +0200

    sorting names in earth model

--- public/Earth.jsx
+++ public/Earth.jsx
@@ -1,20 +1,19 @@
 import React, { useRef } from 'react'
-import { useGLTF, useAnimations } from '@react-three/drei'
+import { useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber';
 
 export default function Earth(props) {
-  const group = useRef()
-  const { nodes, materials, animations } = useGLTF('/earth.gltf')
-  const { actions } = useAnimations(animations, group)
+  const { nodes, materials } = useGLTF('/earth.gltf')
+  const earthRef = useRef()
 
-  // Define a function to update rotation in each frame
-  useFrame((state, delta) => {
-    // Update the rotation of the Earth group to create rotation effect
-    group.current.rotation.y += 0.10 * delta; // Adjust rotation speed as needed
+  useFrame((_, delta) => {
+    if (earthRef.current) {
+      earthRef.current.rotation.y += 0.10 * delta;
+    }
   });
 
   return (
-    <group ref={group} {...props} dispose={null}>
+    <group ref={earthRef} {...props} dispose={null}>
       <group name="Sketchfab_Scene">
         <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]} scale={0.002}>
           <group name="fc09fa6deb6c45f687e724438fed81adfbx" rotation={[Math.PI / 2, 0, 0]}>
