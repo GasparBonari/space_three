9596c2f8771f6de0a4ecced07d6a194b1f844a18
Gaspar Bonari <gasparbonari@gmail.com>
2024-10-20 13:06:14 +0200

    updating saturn model

--- public/Saturn.jsx
+++ public/Saturn.jsx
@@ -1,30 +1,29 @@
 import React from 'react'
 import { useGLTF } from '@react-three/drei'
 
-
 export default function Saturn(props) {
   const { nodes, materials } = useGLTF('/saturn.gltf')
 
   // Clone the rings material and add an emissive property
-  const ringsMaterial = materials.rings.clone()
-  ringsMaterial.emissive = ringsMaterial.color.clone() // Use the original color
-  ringsMaterial.emissiveIntensity = 0.04 // Increase the intensity to make it glow more
+  const ringsMaterial = materials.material.clone() // Assuming 'materials.material' is the rings material
+  ringsMaterial.emissive = ringsMaterial.color.clone() // Use the original color for the emissive effect
+  ringsMaterial.emissiveIntensity = 0.04 // Adjust the emissive intensity to make the rings glow
 
   return (
     <group {...props} dispose={null}>
-      <group scale={0.0015}>
-        <mesh
-          geometry={nodes.saturn_Planet_0.geometry}
-          material={materials.Planet}
-          rotation={[-0.779, 0.194, -0.033]}
-          scale={100}
-        />
-        <mesh
-          geometry={nodes.ring_rings_0.geometry}
-          material={ringsMaterial} // Use the modified rings material
-          rotation={[2.362, -0.198, 0.029]}
-          scale={[-3383.158, 3383.158, 3383.158]}
-        />
+      <group rotation={[-Math.PI / 2, 0, 0]}>
+        <group position={[0, 0, 0]} rotation={[-1.571, -1.386, 0.001]} scale={1.8}>
+          <mesh geometry={nodes.Object_5.geometry} material={ringsMaterial} position={[0.017, 0, 0.001]} rotation={[1.571, -0.002, -1.386]} scale={0.005} />
+          <mesh geometry={nodes.Object_8.geometry} material={ringsMaterial} position={[0.017, 0, 0.001]} rotation={[1.571, -0.002, -1.386]} scale={0.005} />
+          <mesh geometry={nodes.Object_11.geometry} material={ringsMaterial} position={[0.017, 0, 0.001]} rotation={[1.571, -0.002, -1.386]} scale={0.005} />
+          <mesh geometry={nodes.Object_14.geometry} material={ringsMaterial} position={[0.017, 0, 0.001]} rotation={[1.571, -0.002, -1.386]} scale={0.005} />
+          <mesh geometry={nodes.Object_16.geometry} material={ringsMaterial} position={[0.017, 0, 0.001]} rotation={[1.571, -0.002, -1.386]} scale={0.005} />
+        </group>
+        <group scale={1}>
+          <mesh geometry={nodes.Object_20.geometry} material={materials.material_0} scale={0.01} />
+          <mesh geometry={nodes.Object_23.geometry} material={materials.material_0} scale={0.01} />
+          <mesh geometry={nodes.Object_25.geometry} material={materials.material_0} scale={0.01} />
+        </group>
       </group>
     </group>
   )
